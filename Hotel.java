04d06ce770b3886763efb7527c3eb72a368061a6
JAYA2001-04 <91727647+JAYA2001-04@users.noreply.github.com>
2023-03-03 20:03:50 +0530

    Add files via upload
    Jdbc project

--- Hotel.java
+++ Hotel.java
@@ -1,203 +1,79 @@
-package ConsoleBasedproject;
-//In this program we acting has a Restaurant management(eg:Swiggy)to add restaurant details.
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+package Jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
+/* here we are creating method to add ,display,delete,search,update and inside we can give Query*/
+
+
 public class Hotel {
-	/*to hide the details we use private attributes
-	 * resName
-	 * resId
-	 * resownerName
-	 * location
-	 * rating
-	 * veg_nonVeg
-	 */
-	private String resName;
-	private int resId;
-	private String resOwnername;
-	private float rating;
-	private String location;
-	private String veg_nonveg;
-	private static Scanner scan = new Scanner(System.in);
-	/*we create list to add restaurant details
-	 * ArrayList implements list interface
-	 * methods-->add,remove,set,get
-	 */
-	List<Hotel> restaurant = new ArrayList<Hotel>();
-	/*Hotel()-->no parameter constructor is defined--->to display the details to the user
-	 * Hotel(resname,resownername,resId,location,rating,veg_nonveg)-->parameterized constructor is defined
-	 * 
-	 * this keyword-->if the attributes and parameter names are same we use this keyword
-	 * we can access attributes with in the class
-	 */
-	public Hotel() {
-		System.out.println("1.Add the Restaurant ");
-		System.out.println("2.Display all the Restaurant ");
-		System.out.println("3.Delete the Restaurant ");
-		System.out.println("4.Search the Restaurant");
-		System.out.println("5.Update the Restaurant name");
-		
-	}
-	public Hotel(String resName,int resId,String resOwnername,float rating,String location,String veg_nonveg){
-		this.resName= resName;
-		this.resId =resId ;
-		this.resOwnername = resOwnername;
-		this.rating = rating;
-		this.location = location;
-		this.veg_nonveg = veg_nonveg;
-	}
-	// Here we use encapsulation methods to set the input we use setter and getter to get the input
-	public String getresName() {
-		return resName;
-	}
-	public void setresName(String resName) {//to set the input
-		this.resName = resName;
-	}
+	private static Scanner scan =new Scanner(System.in);
+	public void toAdd(Connection connection) throws SQLException {
+		PreparedStatement addAll=connection.prepareStatement("INSERT INTO hotel values(?,?,?,?,?,?)");
+		System.out.println("Enter the restaturant id: ");
+		addAll.setInt(1,scan.nextInt());
+		System.out.println("Enter the restaturant Name: ");
+		addAll.setString(2,scan.next());
+		System.out.println("Enter the owner name: ");
+		addAll.setString(3, scan.next());
+		System.out.println("Enter the location: ");
+		addAll.setString(4, scan.next());
+		System.out.println("Enter the rating: ");
+		addAll.setFloat(5, scan.nextFloat());
+		System.out.println("Enter the foodType: ");
+		addAll.setString(6, scan.next());
+		int insertValues=addAll.executeUpdate();
+		System.out.println(insertValues+" inserted values");
+		}
 	
-	public int getresId() {//to get the the output from setter
-		return resId;
+	public void toDisplayAll(Connection connection) throws SQLException,Exception {
+		Statement inputDisplay=connection.createStatement();
+		ResultSet view=inputDisplay.executeQuery("SELECT * FROM hotel");
+		while(view.next()) {
+			System.out.println("restaturant id= "+view.getInt(1)+", restaturant name= "+view.getString(2)+", Owner name= "+view.getString(3)+", Location= "+view.getString(4)+", Rating= "+view.getFloat(5)+", Food type= "+view.getString(6));	
+		}
+
 	}
-	public void setresId(int resId) {
-		this.resId = resId;
+	public void toDisplayById(Connection connection) throws SQLException {
+		PreparedStatement displayId = connection.prepareStatement("SELECT * FROM hotel WHERE restaturantId = ?");
+		System.out.println("Enter the restaurant id to Display");
+		int id = scan.nextInt();
+		displayId.setInt(1,id);
+		ResultSet view = displayId.executeQuery();
+		while(view.next()) {
+			System.out.println("restaturant ID= "+view.getInt(1)+", restaturant name= "+view.getString(2)+", Owner name= "+view.getString(3)+", Location= "+view.getString(4)+", Rating= "+view.getFloat(5)+", Food type= "+view.getString(6));
+		}
 	}
-	public String getresOwnername() {
-		return resOwnername;
+	public void toDeleteById(Connection connection) throws SQLException {
+		PreparedStatement delete = connection.prepareStatement("DELETE FROM hotel WHERE restaturantId= ?");
+		System.out.println("Enter the restaurant id to Delete");
+		int id = scan.nextInt();
+		delete.setInt(1,id);
+		int deleteValue = delete.executeUpdate();
+		System.out.println(deleteValue + "record deleted");
 	}
-	public void setresownername(String resOwnername) {
-		this.resOwnername = resOwnername;
-	}
-    
-	public float getRating() {
-		return rating;
-	}
-	public void setRating(float rating) {
-		this.rating = rating;
-	}
-	public String getLocation() {
-		return location;
-	}
-	public void setLocation(String location) {
-		this.location = location;
-	}
+	public void toUpdateById(Connection connection) throws SQLException,Exception {
+		         //recrdnum
+				//what are the fields valules
+				//set to pstmt 
+				//execute --> executeUpdate --> 
+				PreparedStatement update=connection.prepareStatement("Update hotel set foodType=? where restaturantId= ?");
+				System.out.println("Enter the hotel Id: ");
+				update.setInt(2, scan.nextInt());
+				System.out.println("Enter the food type: ");
+				update.setString(1, scan.next());
+				int updateValues=update.executeUpdate();
+				System.out.println(updateValues+" record updated successfully");
+				
+			}
+
+
+
+
 	
-	public String getVeg_nonveg() {
-		return veg_nonveg;
-	}
-	public void setVeg_nonveg(String veg_nonveg) {
-		this.veg_nonveg = veg_nonveg;
-	}
-	//@override
-	public String toString() {
-		return resName + " "+ resId +" "+ resOwnername+" "+rating+" "+location+" "+veg_nonveg;
-    }
-	/*add all the restaurant details in this method
-	 *listname.add(new classname(parameters))
-	 */
-	public void insert() {
-		System.out.println("insert the restaurant details");
-		System.out.print("Enter the restaurant name ");
-		String resname = scan.next();
-		System.out.print("Enter the Restaurant Id ");
-		int resId = scan.nextInt();
-		System.out.print("Enter the Restaurant Owner name ");
-		String resOwnername = scan.next();
-		System.out.print("Enter the Restaurant rating ");
-		float rating = scan.nextFloat();
-		System.out.print("Enter the Restaurant location ");
-		String location = scan.next();
-		System.out.print("Enter the Restaurant veg or nonveg ");
-		String veg_nonveg = scan.next();
-		restaurant.add(new Hotel(resname,resId,resOwnername,rating,location,veg_nonveg));
-		System.out.println("Restaurant deatils are added");
-	}
-	//Here we display the inserted restaurant details by using iterator 
-	public void display() {
-		System.out.println("Display the restaurant details");
-		Iterator<Hotel> restaurantlist = restaurant.iterator();
-		while(restaurantlist.hasNext()) {
-			System.out.println(restaurantlist.next());
-		}
-		//System.out.println(resList);
-	}
-	/*here we take unique id number to remove the hotel details
-	 *if the id is not their it will print "Hotel is not found"
-	 *if id is found --> remove the hotel details
-	 */
-	public void remove(Hotel obj) {
-		System.out.println("Delete the details");
-		boolean flag = false;
-		System.out.println("enter the Hotel Id to delete");
-		int delId = scan.nextInt();
-		Iterator<Hotel> restaurantlist1 = restaurant.iterator();
-		while(restaurantlist1.hasNext()) {
-			obj = restaurantlist1.next();//current element store in obj
-			if(obj.getresId()==delId) {
-				flag = true;
-				restaurant.remove(obj);
-				System.out.println("Deleted Successfully");
-				break;
-			}
-		}
-		if(flag == false) {
-			System.out.println("Restaurant Id is not found");
-		}
-	}
-	/*search the hotel using id number
-	if the id is not their it will print -->hotel is not found
-	if id is found-->it display the details searched details
-	 */
-	public void search(Hotel obj) {
-		System.out.println("Search the Restaurant details");
-		boolean flag1 = false;
-		System.out.println("enter the Hotel Id to Search");
-		int foundId = scan.nextInt();
-		Iterator<Hotel> restaurantlist2 = restaurant.iterator();
-		while(restaurantlist2.hasNext()) {
-			obj = restaurantlist2.next();
-			if(obj.getresId()==foundId) {
-				flag1 = true;
-				System.out.println("Found Successfully");
-				break;
-			}
-		}
-		if(flag1 == false) {
-			System.out.println("Restaurant Id is not found");
-		}
-	}
-	//to update the details using id
-	//which index you want set ctr value,add the details
-	public void update(Hotel obj) {
-		System.out.println("update the restaurant details");
-		boolean flag2 = false;
-		System.out.println("Enter the restaurant no to update");
-		int updateId = scan.nextInt();
-		Iterator<Hotel> restaurantlist3 = restaurant.iterator();
-		int ctr = -1;
-		while(restaurantlist3.hasNext()) {
-			ctr++;
-			obj = restaurantlist3.next();//it has all details
-			if(obj.getresId() == updateId) {
-				flag2 = true;
-				System.out.println("Enter the restaurant name");
-				String resname = scan.next();
-				System.out.println("Enter the Restaurant id");
-			    int resId=scan.nextInt();
-				System.out.println("Enter the Restaurant ownername ");
-				String resOwnername=scan.next();
-				System.out.println("Enter the Restaurant rating");
-				float rating=scan.nextFloat();
-				System.out.println("Enter the Restaurant location");
-				String location = scan.next();
-				System.out.println("Enter the Restaurant veg or nonveg");
-				String veg_nonveg = scan.next();
-				restaurant.set(ctr,new Hotel(resname,resId,resOwnername,rating,location,veg_nonveg));
-				break;
-			}
-		}
-		if(flag2 == false) {
-			System.out.println("not updated");
-		}
-	}
+
 }
